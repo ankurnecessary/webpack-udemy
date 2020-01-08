737005bc37c81f551c5229cbd5766a75eb21fea4
Ankur Gupta <ankurnecessary@gmail.com>
2020-01-08 14:32:00 +0530

    Lecture 5: Better Errors and Loaders for HTML & Images @ https://www.udemy.com/course/webpack-beyond-the-basics/learn/lecture/8445444#questions

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -10,7 +10,8 @@ module.exports = {
 		publicPath: "/"
 	},
 	devServer: {
-		contentBase: "dist"
+		contentBase: "dist",
+		overlay: true
 	},
 	module: {
 		rules: [
@@ -22,6 +23,41 @@ module.exports = {
 					},
 					{
 						loader: "css-loader"
+					}
+				]
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						// It decides the name of the file in which HTML to be kept
+						loader: "file-loader",
+						options: {
+							name: '[name].html'
+						}
+					},
+					{
+						// Tells webpack to make a separate file
+						loader: "extract-loader"
+					},
+					{
+						// HTML linting
+						loader: "html-loader",
+						options: {
+							attrs: ["img:src"]
+						}
+					}
+				]
+			},
+			{
+				test: /\.(jpg|gif|png)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "images/[name].[ext]",
+							esModule: false // This is a kind of hack used to remove an issue. So, in future we will remove it
+						}
 					}
 				]
 			}
